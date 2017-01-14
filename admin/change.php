4013eead641fd0ae14e84f6322355869c09d2ae4
Jomar Diamse <jomar.diamse@yahoo.com>
2017-01-14 13:07:39 +0800

    Update change.php

--- admin/change.php
+++ admin/change.php
@@ -2,16 +2,16 @@
 <html>
 <head>
 <?php include_once('header.php'); ?>
-<title>Admin</title>
+<title>Admininstrator Panel</title>
 </head>
 <body>
 	<?php 
 
-	//  判断是否登陆
+	//  Determine whether or not landing
 	if (!(isset($_SESSION["success"]) && $_SESSION["success"] === true)) {
-		//  验证失败，将 $_SESSION["success"] 置为 false
+		//  Validation failed with $ _SESSION ["success"] set to false
 		$_SESSION["success"] = false;
-		echo("您没有登录,正跳转到登录页");
+		echo("You are not logged-in! Redirecting to login page");
 		echo("<meta http-equiv=refresh content='2; url=login.php'>");
 		die();
 	}
@@ -23,26 +23,26 @@
 			bgColor=#ffff99>
 			<TBODY>
 				<TR>
-					<TD align=center height=25>原密码：&nbsp; <INPUT tabIndex=1
+					<TD align=center height=25>Old Password：&nbsp; <INPUT tabIndex=1
 						type=password maxLength=20 size=15 name=oldpassword>
 					</TD>
 				</TR>
 				<TR>
-					<TD align=center>新密码：&nbsp; <INPUT tabIndex=2 type=password
+					<TD align=center>New Password：&nbsp; <INPUT tabIndex=2 type=password
 						maxLength=20 size=15 name=newpassword>
 					</TD>
 				</TR>
 				<TR>
-					<TD align=center>重复新密码：&nbsp; <INPUT tabIndex=3 type=password
+					<TD align=center>Retype New Password: &nbsp; <INPUT tabIndex=3 type=password
 						maxLength=20 size=15 name=renewpassword>
 					</TD>
 				</TR>
 				<TR>
 					<TD align=center height=25><INPUT id=pw_manager
 						style="BORDER-RIGHT: 0px; BORDER-TOP: 0px; BORDER-LEFT: 0px; WIDTH: 65px; CURSOR: hand; BORDER-BOTTOM: 0px; HEIGHT: 18px"
-						type=submit value="修改密码" name=pw_manager> <INPUT id=reset
+						type=submit value="Change Password" name=pw_manager> <INPUT id=reset
 						style="BORDER-RIGHT: 0px; BORDER-TOP: 0px; BORDER-LEFT: 0px; WIDTH: 52px; CURSOR: hand; BORDER-BOTTOM: 0px; HEIGHT: 18px"
-						type=reset value="重置" name=reset>
+						type=reset value="Reset" name=reset>
 					</TD>
 				</TR>
 
