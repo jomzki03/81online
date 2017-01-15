2791e0f6cd13368188933217631760df3e24cfaa
Jomar Diamse <jomar.diamse@yahoo.com>
2017-01-15 13:51:53 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,11 +5,11 @@
 </head>
 <?php
 session_start();
-//  这种方法是销毁整个 Session 文件
-//  这种方法是将原来注册的某个变量销毁
+/ / This method is to destroy the entire Session file
+/ / This method is the original registration of a variable destroyed
 unset($_SESSION['admin']);
 session_destroy();
-echo("您已经登出,正跳转到登录页！");
+echo("Successfully logged out! Redirecting to login page!");
 echo("<meta http-equiv=refresh content='2; url=login.php'>"); 
 ?>
 </html>
